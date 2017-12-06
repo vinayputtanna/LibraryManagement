059b8973029b8f0ade7fb348929662c1f78fd69b
Vinay Puttanna <vinayppn@gmail.com>
2017-12-05 19:42:50 -0800

    Added CheckoutsActivity

---
+++ app/src/main/java/com/example/avdeepsandhu/librarymanagement/CheckoutsActivity.java
@@ -0,0 +1,14 @@
+package com.example.avdeepsandhu.librarymanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckoutsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkouts);
+
+    }
+}
