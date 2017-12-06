9f71552a9f3a6b5c21845f96d6ecc73a4bc3b9ed
Vinay Puttanna <vinayppn@gmail.com>
2017-12-05 23:13:57 -0800

    Added Book class

---
+++ app/src/main/java/com/example/avdeepsandhu/librarymanagement/Book.java
@@ -0,0 +1,107 @@
+package com.example.avdeepsandhu.librarymanagement;
+
+/**
+ * Created by Vinay on 12/5/2017.
+ */
+
+public class Book {
+    private String mAuthor;
+    private String mBookName;
+    private String mCallNumber;
+    private String mPublisher;
+    private String mYearOfPublication;
+    private String mLocation;
+    private String mNumberOfCopies;
+    private String mCurrentStatus;
+    private String mKeywords;
+    private String mCoverImage;
+    private String mDueDate;
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public void setAuthor(String author) {
+        this.mAuthor = author;
+    }
+
+    public String getBookName() {
+        return mBookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.mBookName = bookName;
+    }
+
+    public String getCallNumber() {
+        return mCallNumber;
+    }
+
+    public void setCallNumber(String callNumber) {
+        this.mCallNumber = callNumber;
+    }
+
+    public String getPublisher() {
+        return mPublisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.mPublisher = publisher;
+    }
+
+    public String getYearOfPublication() {
+        return mYearOfPublication;
+    }
+
+    public void setYearOfPublication(String yearOfPublication) {
+        this.mYearOfPublication=yearOfPublication;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public void setLocation(String location) {
+        this.mLocation=location;
+    }
+
+    public String getNumberOfCopies() {
+        return mNumberOfCopies;
+    }
+
+    public void setNumberOfCopies(String numberOfCopies) {
+        this.mNumberOfCopies=numberOfCopies;
+    }
+
+    public String getCurrentStatus() {
+        return mCurrentStatus;
+    }
+
+    public void setCurrentStatus(String currentStatus) {
+        this.mCurrentStatus=currentStatus;
+    }
+
+    public String getKeywords() {
+        return mKeywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.mKeywords = keywords;
+    }
+
+    public String getCoverImage() {
+        return mCoverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.mCoverImage = coverImage;
+    }
+
+    public String getDueDate() {
+        return mDueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.mDueDate= dueDate;
+    }
+}
